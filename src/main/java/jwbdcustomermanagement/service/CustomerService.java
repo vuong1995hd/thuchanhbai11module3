5c7d3a95b63df3e0a6a732558337069bb906fb02
vuong1995hd <hoangvietvuong25@gmail.com>
2024-12-26 09:43:14 +0700

    thmd3

---
+++ src/main/java/jwbdcustomermanagement/service/CustomerService.java
@@ -0,0 +1,18 @@
+package com.jwbdcustomermanagement.service;
+
+
+import com.jwbdcustomermanagement.model.Customer;
+
+import java.util.List;
+
+public interface CustomerService {
+    List<Customer> findAll();
+
+    void save(Customer customer);
+
+    Customer findById(int id);
+
+    void update(int id, Customer customer);
+
+    void remove(int id);
+}
